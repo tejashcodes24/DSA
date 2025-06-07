98911302440358bb35417226798cd5911f6bca31
Tejash <tejashmaru2003@gmail.com>
2025-06-07 12:08:29 +0530

    deletion operation in linked list

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -41,6 +41,50 @@ void insertAtPosition(Node* &head,int pos,int val){
     new_node -> next = temp -> next;
     temp -> next = new_node;
 }
+
+void updateAtPosition(Node* &head, int k, int val){
+    Node* temp = head;
+    int count = 0;
+    while(count != k){
+        temp = temp -> next;
+        count++;
+    }
+
+    temp -> val = val;
+}
+
+void deleteAtHead(Node* &head){
+    Node* temp = head;
+    head = head -> next;
+    free(temp);
+}
+
+void deleteAtTail(Node* &head){
+    Node* second_last = head;
+
+    while(second_last -> next -> next != NULL){
+        second_last = second_last -> next;
+    }
+    Node* temp = second_last -> next;
+    second_last -> next = NULL;
+    free(temp);
+}
+
+void deleteAtPosition(Node* &head, int pos){
+
+    if(pos == 0){
+        deleteAtHead(head);
+        return;
+    }
+    Node * prev = head;
+    for(int i = 0; i < pos - 1; i++){
+        prev = prev -> next;
+    }
+    Node * temp = prev -> next;
+    prev -> next = prev -> next -> next;
+    free(temp);
+}
+
 void display(Node* head){
     Node*temp = head;
     while(temp != NULL){
@@ -59,6 +103,21 @@ int main(){
     insertAtTail(head,4);
     display(head);
     insertAtPosition(head,1,5);
+    display(head);
+    updateAtPosition(head,2,6);
+    display(head);
+
+    deleteAtHead(head);
+    display(head);
+
+    deleteAtTail(head);
+    display(head);
+
+    insertAtHead(head,4);
+    insertAtHead(head,3);
+    display(head);
+
+    deleteAtPosition(head,2);
     display(head);
     
     return 0;
